95cef3bd4a9e3484429b26817a23f46ac4283d7e
Shalom Kazaz <shalom@tikalk.com>
2009-12-29 18:19:11 +0200

    initial commit

---
+++ kiki-reactor/kiki-core/src/main/java/com/tikalk/mvn/sample/kiki/Kiki.java
@@ -0,0 +1,11 @@
+package com.tikalk.mvn.sample.kiki;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: shalom
+ * Date: Dec 29, 2009
+ * Time: 3:07:21 PM
+ */
+public class Kiki {
+    //just to make maven build a jar
+}
---
+++ kuku-reactor/kuku-core/src/main/java/com/tikalk/mvn/sample/kuku/Kuku.java
@@ -0,0 +1,10 @@
+package com.tikalk.mvn.sample.kuku;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: shalom
+ * Date: Dec 29, 2009
+ * Time: 3:07:56 PM
+ */
+public class Kuku {
+}
